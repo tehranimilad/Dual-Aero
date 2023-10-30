9f2ed5a03837cdb707d185333d43896e8224c72b
Milad Tehrani <tehranimilad92@gmail.com>
2023-10-30 15:01:06 -0400

    creating necessary files and adding React Bootstrap

---
+++ frontend/src/pages/Homepage.js
@@ -0,0 +1,14 @@
+import Badge from 'react-bootstrap/Badge';
+
+
+function Homepage() {
+    return (
+      <div className="App">
+        <h1>Welcome to Dual Aero</h1>
+        <img  src="https://i.pinimg.com/564x/ac/e1/8d/ace18d8f5957d6bc3706039c4b97c121.jpg"></img>
+      </div>
+    );
+  }
+  
+  export default Homepage;
+  
