570e05f513af44483477251a00b960d90d16ae6e
Milad Tehrani <tehranimilad92@gmail.com>
2023-10-31 14:29:07 -0400

    revised logo for nav

--- frontend/src/components/nav/nav.js
+++ frontend/src/components/nav/nav.js
@@ -9,14 +9,14 @@ function Navi() {
         <Navbar bg="white" data-bs-theme="light">
             <Container>
             <Navbar.Brand href="#home">
-            {/* <img
-              src="https://i.pinimg.com/564x/ac/e1/8d/ace18d8f5957d6bc3706039c4b97c121.jpg"
-              width="100"
-              height="80"
+            <img
+              src="https://i.ibb.co/WW4Gx7W/Dual-Aero-Skinny.png"
+              width="70"
+              height="50"
+              padding-left="20px"
               alt="Aviation Business Bootstrap logo"
-            /> */}
+            />
           </Navbar.Brand>
-                <Navbar.Brand href="#home">Dual Aero</Navbar.Brand>
                     <Nav className="me-auto">
                         <Nav.Link href="#home">Home</Nav.Link>
                         <Nav.Link href="#about">About Us</Nav.Link>
--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -7,7 +7,9 @@ import "./Homepage.css"
 function Homepage() {
     return (
       <div className="App">
+        <div className="slogan-div">
         <p className="slogan"> Elevate Your Expectations, Trust Dual Aero.</p>
+        </div>
         <Container />
             <Row>
                 <div className="homepage-image" />
