9b1d97c63abbdf746f417da5ddce89639f94eb5f
Milad Tehrani <tehranimilad92@gmail.com>
2023-10-31 11:17:32 -0400

    styling homepage

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -2,18 +2,18 @@ import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
 import Row from 'react-bootstrap/Row';
-
+import "./Homepage.css"
 
 function Homepage() {
     return (
       <div className="App">
-        <h1>Welcome to Dual Aero</h1>
-        <Container>
+        <Container />
             <Row>
-                <Image src="https://images.seattletimes.com/wp-content/uploads/2023/06/06172023_transonic-illo_200022.jpg?d=1536x864" fluid />
+                <div className="homepage-image">
+                <p className="slogan">Elevate Your Expectations, Trust Dual Aero</p>
+                    </div>
+                
             </Row>
-        </Container>
-        
             <Image src="https://i.pinimg.com/564x/ac/e1/8d/ace18d8f5957d6bc3706039c4b97c121.jpg" fluid />
       </div>
     );
